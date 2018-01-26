97a67bad17e4e9bf952de72b437fb839f8cf8ba5
Lyubo <lyubo.velikoff@gmail.com>
2018-01-26 15:40:36 +0000

    refactor: Clean up and set up starting structure.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/* React */
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* Styles */
+import './index.css'
+
+/* Components*/
+import App from './components/app/App'
+
+/* Sergice workers */
+import registerServiceWorker from './service-workers/registerServiceWorker'
+
+/* Redux */
+// import { createStore, applyMiddleware, compose } from 'redux'
+// import reducer from './reducers'
+// import { Provider } from 'react-redux'
+
+/* Render Application */
+ReactDOM.render(<App />, document.getElementById('root'))
+
+/* Register service workers */
 registerServiceWorker();
